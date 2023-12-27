8e19d0c7c51fbced83c378233f8627a51a45262d
Jesús García Garijo <jesus.albarregas@gmail.com>
2023-12-27 21:25:12 +0100

    Eliminar comentarios

--- src/main/webapp/js/cambioAvatar.js
+++ src/main/webapp/js/cambioAvatar.js
@@ -7,8 +7,6 @@ if (typeof imagenInput === 'undefined') {
 $(document).ready(function () {
     let original = $('#previa').attr('src');
     let selloTiempo = new Date();
-//    console.log("Hola mundo");
-//    console.log(selloTiempo);
     $('#previa').attr('src', original + '?' + selloTiempo);
     $('#ficheroAvatar').on('change', function () {
         readURL(this);
@@ -22,8 +20,6 @@ $(document).ready(function () {
         var form = $("#modAvatar")[0];
         var data = new FormData(form);
 
-//        waitingDialog.show('Guardando imagen...');
-
         $("#cambiarAvatar").prop("disabled", true);
 
         console.log("Antes del ajax");
@@ -36,7 +32,6 @@ $(document).ready(function () {
             contentType: false,
             cache: false,
             success: function (data) {
-//                waitingDialog.hide();
 
                 $.notify(data[1], {className: data[0]});
                 if (data[0] !== 'error') {
@@ -56,9 +51,7 @@ function readURL(input) {
         var reader = new FileReader();
         reader.onload = function (e) {
             imagenInput = e.target.result;
-            //let selloTiempo = new Date();
-            //$("#myimg").attr("src", "/myimg.jpg?"+d.getTime());
-            //$('#previa').attr('src', e.target.result+'?'+selloTiempo.getTime());
+            
             $('#previa').attr('src', e.target.result);
         };
         reader.readAsDataURL(input.files[0]);
